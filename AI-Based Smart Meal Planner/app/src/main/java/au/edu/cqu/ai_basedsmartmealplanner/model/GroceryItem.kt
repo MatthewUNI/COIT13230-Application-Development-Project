d06bbcadff171068b8ad4389f3e4132bd6a9bebb
Ansh Patel <12268984@cqumail.com>
2026-08-29 17:34:40 +1000

    Add grocery item data model

--- AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/model/GroceryItem.kt
+++ AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/model/GroceryItem.kt
@@ -1,3 +1,9 @@
 package au.edu.cqu.ai_basedsmartmealplanner.model
 
-data class GroceryItem()
+data class GroceryItem(
+    val name: String,
+    val quantity: Double,
+    val unit: String,
+    val category: String,
+    val isPurchased: Boolean = false
+)
