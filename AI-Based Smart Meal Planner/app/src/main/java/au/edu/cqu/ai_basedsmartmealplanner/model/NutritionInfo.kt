0d5f485332a6c727c65586487f6a5920bd99c4ab
Ansh Patel <12268984@cqumail.com>
2026-08-29 16:43:38 +1000

    Add nutrition information data model

--- AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/model/NutritionInfo.kt
+++ AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/model/NutritionInfo.kt
@@ -1,3 +1,11 @@
 package au.edu.cqu.ai_basedsmartmealplanner.model
 
-data class NutritionInfo()
+data class NutritionInfo(
+    val energyKj: Double,
+    val proteinG: Double,
+    val carbohydratesG: Double,
+    val fatG: Double,
+    val fibreG: Double,
+    val sugarsG: Double,
+    val sodiumMg: Double
+)
