193c127589d5afd5f8eb6e317680f13c6c266b86
Ansh Patel <12268984@cqumail.com>
2026-08-31 22:21:55 +1000

    Add grocery list generator

--- AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/grocery/GroceryListGenerator.kt
+++ AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/grocery/GroceryListGenerator.kt
@@ -1,4 +1,11 @@
 package au.edu.cqu.ai_basedsmartmealplanner.grocery
 
+import au.edu.cqu.ai_basedsmartmealplanner.model.GroceryItem
+import au.edu.cqu.ai_basedsmartmealplanner.model.GroceryList
+
 class GroceryListGenerator {
+
+    fun generateGroceryList(items: List<GroceryItem>): GroceryList {
+        return GroceryList(items = items)
+    }
 }
