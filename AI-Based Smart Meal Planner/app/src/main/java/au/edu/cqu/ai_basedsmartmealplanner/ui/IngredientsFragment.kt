f68541a1cb6d10ea378a32237fa300affbd01f6a
MatthewUNI <matthew.j.haydon@cqumail.com>
2026-09-01 18:26:11 +1000

    Update profile and shared app UI
    Added profile and ingredient management, updated app styling, added Home screen navigation, and prepared basic Meal Plan, Recipes and Grocery List views for component integration.

--- AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/ui/IngredientsFragment.kt
+++
@@ -1,5 +0,0 @@
-package au.edu.cqu.ai_basedsmartmealplanner.ui
-import androidx.fragment.app.Fragment
-import au.edu.cqu.ai_basedsmartmealplanner.R
-
-class IngredientsFragment : Fragment(R.layout.fragment_ingredients)
