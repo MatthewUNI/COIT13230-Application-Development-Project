af2d4fbc6dba40a84ba0018f9921f070b5eb84d0
MatthewUNI <matthew.j.haydon@cqumail.com>
2026-09-01 12:37:56 +1000

    Update UserProfile.kt

--- AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/model/UserProfile.kt
+++ AI-Based Smart Meal Planner/app/src/main/java/au/edu/cqu/ai_basedsmartmealplanner/model/UserProfile.kt
@@ -1,3 +1,11 @@
 package au.edu.cqu.ai_basedsmartmealplanner.model
 
-data class UserProfile()
+data class UserProfile(
+    val profileId: Int,
+    var goalType: String,
+    var currentWeight: Double,
+    var targetWeight: Double,
+    var dietaryRequirements: List<String>,
+    var foodPreferences: List<String>,
+    var availableIngredients: List<String>
+)
