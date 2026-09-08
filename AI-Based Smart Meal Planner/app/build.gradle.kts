0a3742e03f7f67af943f96643b4afcd529656cdb
MatthewUNI <matthew.j.haydon@cqumail.com>
2026-09-08 14:42:16 +1000

    Update build.gradle.kts

--- AI-Based Smart Meal Planner/app/build.gradle.kts
+++ AI-Based Smart Meal Planner/app/build.gradle.kts
@@ -55,20 +55,22 @@ dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
+
     // Navigation components
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 
-    // Retrofit, Gson, and Coroutines for Gemini API pipeline
+    // Room database
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
+    // Gemini API pipeline
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.kotlinx.coroutines.android)
 
-    // Unit and Instrumentation Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
